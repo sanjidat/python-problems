2cc827e336b8eef2918720bb542c17a1ecb4309a
sanjidat <sanjidaorin11@gmail.com>
2021-10-22 11:51:05 +0200

    problem10.py

--- problem10.py
+++
@@ -1,8 +0,0 @@
-#Write a program to print multiplication table of a given number
-
-def mul(x):
-    m=0
-    for i in range(1,11):
-        m=x*i
-        print(m)
-mul(int(input("Enter a number:  ")))
